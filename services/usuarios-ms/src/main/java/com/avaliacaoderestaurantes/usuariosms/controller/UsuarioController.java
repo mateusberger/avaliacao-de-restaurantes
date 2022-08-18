779a1f8cd9f8ef447668910bda15f1069ad885f7
Mateus Berger <mateusberger96@gmail.com>
2022-08-17 23:06:32 -0300

    Corrigido o recebimento da paginação no metodo de listagem dos usuários

--- services/usuarios-ms/src/main/java/com/avaliacaoderestaurantes/usuariosms/controller/UsuarioController.java
+++ services/usuarios-ms/src/main/java/com/avaliacaoderestaurantes/usuariosms/controller/UsuarioController.java
@@ -4,12 +4,11 @@ import com.avaliacaoderestaurantes.usuariosms.service.UsuarioService;
 import com.avaliacaoderestaurantes.usuariosms.service.UsuarioService.UsuarioDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Optional;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/usuario")
@@ -20,40 +19,26 @@ public class UsuarioController {
 
     @GetMapping
     public Page<UsuarioDto> listarUsuarios(
-        @PageableDefault(page = 0, size = 10) Pageable paginacao
-    ){
+            @RequestParam(defaultValue = "0") Integer pagina,
+            @RequestParam(defaultValue = "20") Integer tamanhoPagina
+    ) {
+        Pageable paginacao = PageRequest.of(pagina, tamanhoPagina);
         return usuarioService.listarUsuarios(paginacao);
     }
 
     @GetMapping("/{id}")
     public UsuarioDto getUsuarioPeloId(
             @PathVariable Long id
-    ){
+    ) {
         return usuarioService.getUsuarioPorId(id);
     }
 
     @PostMapping
     public UsuarioDto inserirUsuario(
             @RequestBody @Valid UsuarioService.NovoUsuarioForm novoUsuarioForm
-    ){
+    ) {
         return usuarioService.criarNovoUsuario(novoUsuarioForm);
     }
-
-//    @PutMapping("/{id}")
-//    public UsuarioDto atualizarUsuario(
-//            @PathVariable Long id,
-//            @RequestBody Usuario usuario
-//    ){
-//        return null;
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public UsuarioDto atualizarRestaurante(
-//            @PathVariable Long id,
-//            @RequestBody HashMap<Object, Object> fields
-//    ){
-//        return null;
-//    }
 
     @DeleteMapping("/{id}")
     public UsuarioDto deletarUsuario(
